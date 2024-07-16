3a77cd15966457a67fba4059b01006fc2947c53c
Roland Nagy <114018162+rolcho@users.noreply.github.com>
2024-07-16 23:44:55 +0200

    db fix
    Fix sql statement imageUrl in getPosts()

--- code/05-data-mutation/06-image-upload/lib/posts.js
+++ code/05-data-mutation/06-image-upload/lib/posts.js
@@ -55,7 +55,7 @@ export async function getPosts(maxNumber) {
   }
 
   const stmt = db.prepare(`
-    SELECT posts.id, image_url AS image, title, content, created_at AS createdAt, first_name AS userFirstName, last_name AS userLastName, COUNT(likes.post_id) AS likes, EXISTS(SELECT * FROM likes WHERE likes.post_id = posts.id and likes.user_id = 2) AS isLiked
+    SELECT posts.id, image_url AS imageUrl, title, content, created_at AS createdAt, first_name AS userFirstName, last_name AS userLastName, COUNT(likes.post_id) AS likes, EXISTS(SELECT * FROM likes WHERE likes.post_id = posts.id and likes.user_id = 2) AS isLiked
     FROM posts
     INNER JOIN users ON posts.user_id = users.id
     LEFT JOIN likes ON posts.id = likes.post_id
