89e117799eeea64ea53cfa4d9cdd137cf75ea4f6
Ashan Thotawatta <talakshith@gmail.com>
2025-02-15 02:43:51 +0100

    created initial about page

--- code/01-getting-started/01-starting-project/app/page.js
+++ code/01-getting-started/01-starting-project/app/page.js
@@ -1,9 +1,12 @@
+import Link from 'next/link';
 export default function Home() {
+  console.log('Home page rendered');  
   return (
     <main>
       <img src="/logo.png" alt="A server surrounded by magic sparkles." />
       <h1>Welcome to this NextJS Course!</h1>
       <p>🔥 Let&apos;s get started! 🔥</p>
+      <p><Link href="/about">About</Link></p>
     </main>
   );
 }
