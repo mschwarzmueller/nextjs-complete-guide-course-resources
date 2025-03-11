16a99aba41de46fe45c067366282fbeaaba095c8
Ashan Thotawatta <talakshith@gmail.com>
2025-03-11 17:36:04 +0100

    made active the nav button

--- attachments/02-nextjs-essentials/05-onwards-foodies-starting-project/app/components/main-header/main-header.js
+++ attachments/02-nextjs-essentials/05-onwards-foodies-starting-project/app/components/main-header/main-header.js
@@ -1,10 +1,15 @@
+'use client';
 import Link from "next/link";
 import Image from "next/image";
 import logoImg from "@/assets/logo.png";
 import classes from "./main-header.module.css";
 import MainHeaderBackground from "./main-header-background";
+import { usePathname } from "next/navigation";
+
 
 export default function MainHeader() {
+  const path = usePathname();
+  console.log(path);
   return (
     <>
       <MainHeaderBackground />
@@ -16,10 +21,10 @@ export default function MainHeader() {
         <nav className={classes.nav}>
           <ul>
             <li>
-              <Link href="/meals">Browse Meals</Link>
+              <Link href="/meals" className={path.startsWith('/meals') ? classes.active : undefined}>Browse Meals</Link>
             </li>
             <li>
-              <Link href="/meals">Foodies Community</Link>
+              <Link href="/community" className={path.startsWith('/community') ? classes.active: undefined}>Foodies Community</Link>
             </li>
           </ul>
         </nav>
